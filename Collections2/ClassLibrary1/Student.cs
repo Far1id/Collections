7dbbe38da28dd1942958f930f1e6c63c0fb3e8ef
Far1id <7k7u1q0@code.edu.az>
2022-04-08 12:06:08 +0400

    Created Hometask file

---
+++ Collections2/ClassLibrary1/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        public int No { get; set; }
+        public string FullName { get; set; }
+        Dictionary<string, double> Exams = new Dictionary<string, double>();
+
+        public bool CheckExamByExamName(string examname)
+        {
+            foreach (var item in Exams)
+            {
+                if (item.Key == examname)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void AddExam(string examName, double point)
+        {
+            if (CheckExamByExamName(examName) == false)
+            {
+                Exams.Add(examName, point);
+            }
+        }
+        public double GetExamResult(string examName)
+        {
+            foreach (var item in Exams)
+            {
+                if (item.Key == examName)
+                {
+                    return item.Value;
+                }
+            }
+            Console.WriteLine("Bele bir imtahan olmayib.");
+            return -1;
+        }
+        public double GetExamAvg()
+        {
+            int count = 0;
+            double sum = 0;
+            foreach (var item in Exams)
+            {
+                count++;
+                sum += item.Value;
+            }
+            return sum / count;
+        }
+        public void GetInfo()
+        {
+            foreach (var item in Exams)
+            {
+                Console.WriteLine(item.Key + item.Value);
+            }
+        }
+        public void RemoveExamByExamName(string examname)
+        {
+            foreach (var item in Exams)
+            {
+                if (item.Key==examname)
+                {
+                    Exams.Remove(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Collections2/Collections/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> Students = new List<Student>();
+
+            int answer;
+            
+            do
+            {
+                Console.WriteLine("============M E N U===========");
+                Console.WriteLine("1. Telebe elave et");
+                Console.WriteLine("2. Telebeye imtahan elave et");
+                Console.WriteLine("3. Telebenin bir imtahan balına bax");
+                Console.WriteLine("4. Telebenin bütün imtahanlarını göster");
+                Console.WriteLine("5. Telebenin imtahan ortalamasını göster");
+                Console.WriteLine("6. Telebeden imtahan sil");
+                Console.WriteLine("0. Proqramı bitir\n\n");
+                Console.WriteLine("Secim edin: ");
+                answer = Convert.ToInt32(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 1:
+                        bool check = false;
+                        Console.WriteLine("Telebenin fullname ni daxil edin: ");
+                        string fullname = Console.ReadLine();
+                        Console.WriteLine("Telebenin No deyerini daxil edin : (Diqqet!!! Eyni no lu telebe ola bilmez!)");
+                        int no = Convert.ToInt32(Console.ReadLine());
+                        foreach (var item in Students)
+                        {
+                            if (item.No == no)
+                            {
+                                check = true;
+                            }
+                        }
+                        if (!check)
+                        {
+                            Student student = new Student();
+                            student.No = no;
+                            student.FullName = fullname;
+                            Students.Add(student);
+                        }
+                        else
+                            Console.WriteLine("eyni no lu telebe yaradila bilmez.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Telebenin no sunu daxil edin: ");
+                        int no2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Imtahanin adini daxil edin: ");
+                        string examname = Console.ReadLine();
+                        Console.WriteLine("Imtahan pointini daxil edin: ");
+                        double point1 = Convert.ToDouble(Console.ReadLine());
+                        Students[no2].AddExam(examname, point1);
+                        break;
+                    case 3:
+                        Console.WriteLine("Telebenin nosunu daxil edin: ");
+                        int no3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Imtahanin adini daxil edin: ");
+                        string examname2 = Console.ReadLine();
+                        Console.WriteLine(Students[no3].GetExamResult(examname2)); 
+                        break;
+                    case 4:
+                        Console.WriteLine("Telebenin no sunu daxil edin: ");
+                        int no4 = Convert.ToInt32(Console.ReadLine());
+                        Students[no4].GetInfo();
+                        break;
+                    case 5:
+                        Console.WriteLine("Telebenin no sunu daxil edin: ");
+                        int no5 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(Students[no5].GetExamAvg());
+                        break;
+                    case 6:
+                        Console.WriteLine("Telebenin no sunu daxil edin: ");
+                        int no6 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Imtahanin adini daxil edin: ");
+                        string examname3 = Console.ReadLine();
+                        Students[no6].RemoveExamByExamName(examname3);
+                        break;
+                    default:
+                        break;
+                }
+            } while (answer!=0);
+        }
+        
+    }
+}
